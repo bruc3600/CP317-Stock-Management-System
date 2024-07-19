07a8b07af917ccde287bf8d7facae1ae36ddeff6
bruc3600 <bruc3600@mylaurier.ca>
2024-07-19 14:06:37 -0400

    testing plot graphic for TSLA stock

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import pandas_datareader as data
 import yfinance as yf
 from datetime import datetime
 
-start = '2019-01-01'
-end = datetime.now().strftime('%Y-%m-%d')
+start = '2019-01-01' # from specified date
+end = datetime.now().strftime('%Y-%m-%d') # to todays date
 
 try:
-#    df = data.DataReader('TSLA', 'yahoo', start, end)
-    df = yf.download('TSLA', start=start, end=end)
+    df = yf.download('TSLA', start=start, end=end) # pull TSLA stock as start point, from specified start to end date
 except Exception as e:
-    print("Failed to fetch data:", e)
+    print("Failed to fetch data:", e) # in case of error, print exception rather than crashing.
 else:
-    print(df.tail())
+    print(df.tail()) # tail = newer dates, head = older dates
+
+# plot
+
+plt.figure(figsize=(10, 5))
+plt.plot(df['Close'], label='TSLA Close Price')
+plt.title('TSLA Stock Close Price Over Time')
+plt.xlabel('Date')
+plt.ylabel('Close Price (USD)')
+plt.legend()
+plt.show()
