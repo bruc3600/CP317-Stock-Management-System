460502704fa4a63be3328a0af8c77d2b60c88925
bruc3600 <bruc3600@mylaurier.ca>
2024-07-26 15:18:39 -0400

    prediction model complete and working on front-end

--- test.py
+++ test.py
@@ -30,6 +30,7 @@ def create_site():
     st.subheader("Raw data")
     st.write(data.tail())
     plot_raw_data(data)
+    forecast(data, period)
 
 
 
@@ -49,20 +50,45 @@ def preprocess_data(data):
     #data = data.drop(['Date', 'Adj Close'], axis = 1)
     return data
 
+# plots open and close raw prices
 def plot_raw_data(data):
+    # to avoid error, ensure there is data to plot
     if data is not None:
         if 'Date' in data.columns:
-            figure = go.Figure()
-            figure.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
-            figure.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
-            figure.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
-            st.plotly_chart(figure)
+            figure = go.Figure() # create figure
+            figure.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open')) #add trace for 'Open' stock prices
+            figure.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close')) # and 'Close' stock prices
+            figure.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True) # give figure a title and slider
+            st.plotly_chart(figure) # display figure on the front end
         else:
             st.error("The 'Date' column is missing from the data")
     else:
         st.error("Data is not available.")
 
+# generates forecast and displays on front end
+def forecast(data, period):
+    data_train = data[['Date', 'Close']] # set 'Date' and 'Close' as the columns from the data
+    data_train = data_train.rename(columns={"Date": "ds", "Close": "y"}) # rename the columns to match requirements for Prophet
+    model = Prophet() # init model
+    model.fit(data_train) # fit the model with the training data
+
+    future = model.make_future_dataframe(periods=period) # create dataframe to hold future dates for prediction (using period variable from slider)
+    forecast = model.predict(future) # generate predictions for dataframe
+
+    # display on front end
+    st.subheader('Forecast data') 
+    st.write(forecast.tail()) # shows just the forecast data in table
+
+    # plot forecast data using plotly
+    st.write('Forecast Data Plot')
+    forecastfigure1 = plot_plotly(model, forecast) 
+    st.plotly_chart(forecastfigure1)
+
+    # plot forecast components
+    st.write('Forecast components')
+    figure2 = model.plot_components(forecast)
+    st.write(figure2)
 
 
-
+# for now this is main function that runs (calls sub functions)
 create_site()
